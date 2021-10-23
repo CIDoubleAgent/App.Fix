2e3943f0f5990eb28bd00ef3f03518fb05fa9db6
Nick Zabkar <nzabkar51@gmail.com>
2021-10-23 13:15:23 -0400

    Added homeRoutes.js and main + homepage handlebars

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize/types");
+const { UPSERT } = require("sequelize/types/lib/query-types");
+
+User.init({
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+})
