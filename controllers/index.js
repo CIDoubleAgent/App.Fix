6c8fee9f90d79ec3c8b8a84ab43c8d69d745da7a
Nick Zabkar <nzabkar51@gmail.com>
2021-11-01 14:32:34 -0400

    Added index.js to controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const authRoutes = require('./api/authRoutes');
+const blogRoutes = require('./api/blogRoutes');
+const dashRoutes = require('./api/dashRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/auth', authRoutes);
+router.use('/blog', blogRoutes);
+router.use('/dash', dashRoutes);
 
 module.exports = router;
