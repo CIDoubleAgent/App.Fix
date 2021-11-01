c8d69b754d9e11f86422007d6706798454924fe6
Nick Zabkar <nzabkar51@gmail.com>
2021-11-01 14:20:02 -0400

    dded seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,11 @@
+const sequelize = require('../config/connection');
+const { User, Blog } = require('../models');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await Blog.sync();
+    await User.sync();
+
+    const blog = await Blog.bulkCreate([{ contents:"These are the contents of a blog post", postTitle: "Post one", userName: "AvidPoster", dateCreated: "1635518563100"  }]);
+}
