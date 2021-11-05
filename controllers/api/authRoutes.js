2f5e7b4e3a1f1dfd7f413b5a65c2871a81d77ab1
Nick Zabkar <nzabkar51@gmail.com>
2021-11-04 23:43:02 -0400

    Added signUp routes middleware for signIn route

--- controllers/api/authRoutes.js
+++ controllers/api/authRoutes.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
+const User = require('../../models/User');
 
 router.get("/signIn", (req, res) => {
     res.render("signIn.handlebars")
 });
 
-router.post("/signIn", (req, res) => {
-    res.render("signIn.handlebars")
+router.post("/signIn", async (req, res) => {
+    console.log(req.body);
+    let { username, password } = req.body;
+    let userData = await User.findOne({where: {username}});
+    if (userData == null) {
+        console.log('Login failed');
+        res.redirect("/auth/signIn");
+    } else if (password == userData.password) {
+        res.redirect("/dash");
+        // TODO: send the user a cookie
+    } else {
+        console.log('Login failed');
+        res.redirect("/auth/signIn");
+    }
+});
+
+router.get("/signUp", (req, res) => {
+    res.render("signUp.handlebars");
+});
+
+router.post("/signUp", (req, res) => {
+    let { username, password } = req.body;
+    User.create({ username, password })
+    .then(function() {
+        res.redirect("/auth/signIn");
+    }).catch(function(err) {
+        console.error(err);
+        res.redirect("/auth/signUp");
+    })
 });
 
 module.exports = router;
