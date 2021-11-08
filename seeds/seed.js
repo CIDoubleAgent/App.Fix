d044c363dd26262c7ea7e7f3f1723dff469e6ae0
Nick Zabkar <nzabkar51@gmail.com>
2021-11-08 05:21:45 -0500

    Changed more instances of "blog" to "post"

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,13 @@
 const sequelize = require('../config/connection');
-const { User, Blog } = require('../models');
+const { User, Post } = require('../models');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    await Blog.sync();
+    await Post.sync();
     await User.sync();
 
-    const blog = await Blog.bulkCreate([{ contents:"These are the contents of a blog post", postTitle: "Post one", username: "thefastestman", dateCreated: new Date() }]);
+    const post = await Post.bulkCreate([{ contents:"These are the contents of a post", postTitle: "Post one", username: "thefastestman", dateCreated: new Date() }]);
     const user = await User.bulkCreate([{ username:"thefastestman", email:"dfastman@fakemail.com", password:"password" }])
 };
 
