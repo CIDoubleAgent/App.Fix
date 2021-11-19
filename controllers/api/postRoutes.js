e8a0eacd849b4b02a0c0ea1899acfd4d031805eb
Nick Zabkar <nzabkar51@gmail.com>
2021-11-19 08:58:53 -0500

    Added post route to postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
+const Post = require('../../models/Post');
 
 router.get("/", (req, res) => {
     res.render("post.handlebars")
 });
 
 router.post("/", (req, res) => {
-    res.render("post.handlebars")
+    const { postTitle, postContents } = req.body
+    Post.create({postTitle, postContents, user_id:req.session.user_id})
+    res.redirect("/dash")
 });
 
 router.get("/:id", (req, res) => {
