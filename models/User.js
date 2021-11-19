07b3aeef526c72441ca8c982f8f01875275b0f84
Nick Zabkar <nzabkar51@gmail.com>
2021-11-19 12:58:11 -0500

    Added bcrypt password check

--- models/User.js
+++ models/User.js
@@ -1,28 +1,47 @@
 const { DataTypes, Model } = require("sequelize");
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {};
-
-User.init({
-    username: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false
-    },
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
     }
-}, { sequelize, modelName: "User" });
+}
+
+User.init(
+    {
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            }
+        }
+
+    }, 
+    { 
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
 
 module.exports = User;
