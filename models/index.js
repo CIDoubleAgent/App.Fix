4c062c49736152901bbcf5adbf0fcd0a3b3391a8
Nick Zabkar <nzabkar51@gmail.com>
2021-11-19 08:59:54 -0500

    Redefined table associations to include comments

--- models/index.js
+++ models/index.js
@@ -1,14 +1,31 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
+const { hasOne } = require('./User');
 
 User.hasMany(Post, {
-    as: "posts",
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
-  
-  Post.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-module.exports = { User, Post };
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+module.exports = { User, Post, Comment };
